077f3ac6cbe52a7acb49f0a7684c2cb318f88c05
Chris Klimas <1405578+klembot@users.noreply.github.com>
2013-04-06 10:00:12 -0400

    Passages save their position

--- js/views/passageitemview.js
+++ js/views/passageitemview.js
@@ -2,10 +2,25 @@
 
 PassageItemView = Marionette.ItemView.extend({
 	template: '#templates .passageItemView',
+	className: 'passage',
 
 	initialize: function()
 	{
 		this.model.on('change', this.render, this);
+	},
+
+	onRender: function()
+	{
+		// have to set absolute positioning manually,
+		// or draggable() will manually apply absolute for us
+
+		this.$el.css({
+			position: 'absolute',
+			top: this.model.get('top'),
+			left: this.model.get('left')
+		});
+
+		this.$el.draggable({ cursor: 'move', addClasses: false });
 	},
 
 	serializeData: function()
@@ -28,6 +43,14 @@ PassageItemView = Marionette.ItemView.extend({
 		'click .delete': function()
 		{
 			this.model.destroy();
+		},
+
+		'dragstop': function (e, ui)
+		{
+			this.model.save({
+				top: ui.position.top,	
+				left: ui.position.left	
+			});
 		},
 
 		'click .edit': 'edit',
