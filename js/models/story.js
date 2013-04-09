305bdf8d537b512d98e7ce891014ed21f4021d9a
Chris Klimas <1405578+klembot@users.noreply.github.com>
2013-04-08 20:17:09 -0400

    Stories remember their starting passage

--- js/models/story.js
+++ js/models/story.js
@@ -1,13 +1,15 @@
 var Story = Backbone.Model.extend({
 	defaults:
 	{
-		name: 'Untitled Story'
+		name: 'Untitled Story',
+		startPassage: -1
 	},
 
 	publish: function()
 	{
 		var passages = app.passages.where({ story: this.cid });
-		var result = '<article data-name="' + this.get('name') + '"><h1>' + this.get('name') + '</h1>';
+		var result = '<article data-name="' + this.get('name') + '" data-start-passage="' +
+					 this.get('startPassage') + '"><h1>' + this.get('name') + '</h1>';
 
 		for (var i = 0; i < passages.length; i++)
 			result += passages[i].publish();
