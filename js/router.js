9c316bee0e85f50f6cf85cdc951e803930a1a4ab
Chris Klimas <chris@twofoldsecret.com>
2013-04-12 21:12:50 -0400

    Save story archive
    This shouldn't be a route -- I guess an app method?

--- js/router.js
+++ js/router.js
@@ -29,13 +29,31 @@ StorybookRouter = Backbone.Router.extend({
 
 		'stories/:id/publish': function (id)
 		{
-			// publish a particular story
+			// publish a particular story to disk
 
 			app.stories.fetch();
 			app.passages.fetch();
 			var story = app.stories.get(id);
 			var blob = new Blob([defaultTemplate.publish(story)], { type: 'text/html;charset=utf-8' });
 			saveAs(blob, story.get('name') + '.html');
+		},
+
+		'archive': function()
+		{
+			// publish an archive of all stories to disk
+
+			app.stories.fetch();
+			app.passages.fetch();
+
+			var output = '';
+
+			app.stories.each(function (story)
+			{
+				output += story.publish() + '\n\n';
+			});
+
+			var blob = new Blob([output], { type: 'text/html;charset=utf-8' });
+			saveAs(blob, new Date().toLocaleString().replace(/[\/:\\]/g, '.') + ' Twine Archive.html');
 		},
 
 		'*path': function()
