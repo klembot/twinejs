1c46befa9998f9b808bb0547ae1a96e264aa0d73
Chris Klimas <chris@twofoldsecret.com>
2013-10-16 21:47:00 -0400

    Add Grunt release task to minify JS/CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,13 +23,49 @@ module.exports = function (grunt)
 
 			build:
 			{
-				files: { 'test.html': 'app.html' }
+				files: { 'index.html': 'app.html' }
 			}
+		},
+
+		clean:
+		{
+			dist: ['dist/']
+		},
+
+		copy:
+		{
+			html:
+			{
+				files:
+				[
+					{ expand: true, src: ['index.html'], dest: 'dist/' }
+				]
+			},
+
+			resources:
+			{
+				files:
+				[
+					{ expand: true, src: ['img/*.png'], dest: 'dist/rsrc/' },
+					{ expand: true, cwd: 'lib/fontawesome/font', src: ['*'], dest: 'dist/rsrc/font/' }
+				]
+			}
+		},
+
+		useminPrepare:
+		{
+			html: 'index.html',
+			options: { dest: 'dist' }
+		},
+
+		usemin:
+		{
+			html: 'dist/index.html'
 		},
 
 		watch:
 		{
-			scripts:
+			templates:
 			{
 				files: ['app.html', 'templates/*.html'],
 				tasks: ['default']
@@ -38,6 +74,16 @@ module.exports = function (grunt)
 	});
 
 	grunt.loadNpmTasks('grunt-bake');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-usemin');
 	grunt.registerTask('default', ['bake']);
+	grunt.registerTask('release',
+	[
+		'clean', 'bake', 'copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin'
+	]);
 };
