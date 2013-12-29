0015e99d8bc7772b062d6b63cb7eb2c737ecc0d7
Ross Smith <evolutioneer@gmail.com>
2013-12-28 19:10:31 -0500

    Added drawLinks() call after passage item drag ends

---
+++ js/app.js
@@ -0,0 +1,225 @@
+/**
+ The main Backbone app running the show. This is accessible via the
+ global variable `window.app`.
+ 
+ @module Twine
+ @class TwineApp
+ @extends Backbone.Marionette.Application
+**/
+
+TwineApp = Backbone.Marionette.Application.extend(
+{
+	/**
+	 Name of the app.
+
+	 @property name
+	**/
+
+	name: 'Twine',
+
+	/**
+	 Version number of the app.
+
+	 @property version
+	**/
+
+	version: '2.0a',
+
+	/**
+	 Synchronizes all stories and passages in memory with what's been stored.
+
+	 @method sync
+	 @param {Function} callback Callback function to invoke once the sync is done.
+	**/
+
+	sync: function (callback)
+	{
+		var self = this;
+
+		this.stories.fetch({
+			success: function()
+			{
+				self.passages.fetch({
+					success: function() { if (callback) callback() }
+				});
+			}
+		});
+	},
+
+	/**
+	 Publishes a story to a file to be downloaded by binding it with the
+	 runtime template. 
+
+	 @method publishStory
+	 @param {Story} story Story model to publish.
+	**/
+
+	publishStory: function (story)
+	{
+		RuntimeTemplate.publish(story, function (html)
+		{
+			var blob = new Blob([html], { type: 'text/html;charset=utf-8' });
+			saveAs(blob, story.get('name') + '.html');
+		});
+	},
+
+	/**
+	 Saves an archive of all stories to a file to be downloaded.
+
+	 @method saveArchive
+	**/
+
+	saveArchive: function()
+	{
+		var output = '';
+		var stories = this.stories.toArray();
+		var i = 0;
+
+		function archiveStory()
+		{
+			// output, stories, and i are defined above
+			
+			stories[i].publish(function (html)
+			{
+				output += html + '\n\n';
+				
+				if (++i < stories.length)
+					archiveStory();
+				else
+				{
+					var blob = new Blob([output], { type: 'text/html;charset=utf-8' });
+					saveAs(blob, new Date().toLocaleString().replace(/[\/:\\]/g, '.') + ' Twine Archive.html');
+				}
+			});
+		};
+
+		archiveStory();
+	},
+
+	/**
+	 Imports a file containing either a single published story, or an
+	 archive of several stories. The stories are immediately saved to storage.
+	 This does not yet work with stories published by Twine 1.x.
+
+	 @method importFile
+	 @param {String} data Contents of the file to be imported.
+	**/
+
+	importFile: function (data)
+	{
+		// parse data into a DOM
+
+		var parsed = $('<html>');
+		var count = 0;
+
+		// remove surrounding <html>, if there is one
+
+		if (data.indexOf('<html>') != -1)
+			parsed.html(data.substring(data.indexOf('<html>') + 6, data.indexOf('</html>')));
+		else
+			parsed.html(data);
+
+		parsed.find('[data-role="twinestory"]').each(function()
+		{
+			var $story = $(this);
+			var startPassageId = $story.attr('data-startnode');
+
+			// create a story object
+
+			var story = window.app.stories.create({ name: $story.attr('data-name') });
+
+			// and child passages
+
+			$story.find('[data-role="passage"]').each(function()
+			{
+				var $passage = $(this);
+				var posBits = $passage.attr('data-twine-position').split(',');
+
+				passage = window.app.passages.create(
+				{
+					name: $passage.attr('data-name'),
+					text: $passage.html(),
+					story: story.id,
+					left: parseInt(posBits[0]),
+					top: parseInt(posBits[1])
+				});	
+
+				if ($passage.attr('data-id') == startPassageId)
+					story.save({ startPassage: passage.id });
+			});
+
+			// for now, glom all style nodes into the stylesheet property
+
+			var stylesheet = '';
+
+			$story.find('[data-role="stylesheet"]').each(function()
+			{
+				stylesheet += $(this).text() + '\n';
+			});
+
+			// likewise for script nodes
+
+			var script = '';
+
+			$story.find('[data-role="script"]').each(function()
+			{
+				script += $(this).text() + '\n';
+			});
+
+			if (stylesheet != '' || script != '')
+				story.save({ stylesheet: stylesheet, script: script });
+
+			count++;
+		});
+
+		return count;
+	}
+});
+
+window.app = new TwineApp();
+
+window.app.addInitializer(function (options)
+{
+	/**
+	 The master collection of all stories.
+
+	 @property stories
+	 @type StoryCollection
+	**/
+
+	app.stories = new StoryCollection();
+
+	/**
+	 The master collection of all passages. This is not differentiated
+	 by story at all -- you'd need to query by a parent story's ID.
+
+	 @property passages
+	 @type PassageCollection
+	**/
+
+	app.passages = new PassageCollection();
+	app.sync();
+
+	/**
+	 The app router.
+
+	 @property router
+	 @type TwineRouter
+	**/
+
+	app.router = new TwineRouter();
+	Backbone.history.start();
+});
+
+window.app.addRegions(
+{
+	/**
+	 The top-level container for views.
+
+	 @property mainRegion
+	**/
+
+	mainRegion: '#regions .main'
+});
+
+window.app.start();
---
+++ js/views/storylistview.js
@@ -0,0 +1,156 @@
+/**
+ Shows a list of stories. Each list item is managed by a StoryItemView.
+
+ @class StoryListView
+ @extends Backbone.Marionette.CompositeView
+**/
+
+StoryListView = Backbone.Marionette.CompositeView.extend(
+{
+	itemView: StoryItemView,
+	itemViewContainer: 'tbody',
+	template: '#templates .storyListView',
+
+	onRender: function()
+	{
+		var self = this;
+
+		// enable tooltips
+
+		this.$('a[title], button[title]').tooltip();
+
+		// enable table sorting
+
+		this.$('table.stories').tablesorter();
+
+		// enable popovers
+
+		this.$('button.addStory')
+		.popover({
+			html: true,
+			placement: 'bottom',
+			content: function() { return $('#addStoryDialog').html() }
+		})
+		.click(function()
+		{
+			$('.popover .newName').focus();
+		});
+
+		this.$el.on('click', 'button.cancelAdd', function()
+		{
+			self.$('.addStory').popover('hide');
+		});
+
+		this.$('button.importStory')
+		.popover({
+			html: true,
+			placement: 'bottom',
+			content: function() { return $('#importStoryDialog').html() }
+		});
+
+		this.$el.on('click', 'button.cancelImport', function()
+		{
+			self.$('.importStory').popover('hide');
+		});
+
+		// force popover content to hide completely
+		// otherwise, inputs would still steal focus --
+		// they were set at opacity 0 but display: block still
+
+		this.$el.on('hidden.bs.popover', function (e)
+		{
+			self.$('.popover').hide();
+		});
+
+		// force only one popover visible at a time
+
+		this.$el.on('show.bs.popover', function (e)
+		{
+			self.$('.pop').not(e.target).popover('hide');	
+		});
+
+		// delete popover is set up in StoryItemView
+	},
+
+	/**
+	 Adds a new story with the name entered in the view's input.newName field.
+	 TODO: prevent duplicate story names
+
+	 @method addStory
+	**/
+
+	addStory: function()
+	{
+		this.collection.create({ name: this.$('input.newName').val() });
+		this.$('.addStory').popover('hide');
+	},
+
+	/**
+	 Saves an archive of all stories by passing the request onto TwineApp.saveArchive().
+
+	 @method saveArchive
+	**/
+
+	saveArchive: function()
+	{
+		window.app.saveArchive();
+	},
+
+	/**
+	 Tries to import the file indicated by the view's input.importFile field.
+	 The result, either success or failure, is shown above the story table.
+	 
+	 @method importFile
+	**/
+
+	importFile: function (e)
+	{
+		var self = this;
+		var reader = new FileReader();
+
+		reader.onload = function (e)
+		{
+			var className = '';
+			var message = '';
+
+			try
+			{
+				var count = window.app.importFile(e.target.result);
+
+				if (count > 0)
+				{
+					className = 'alert-success';
+
+					if (count == 1)
+						message = '1 story was imported.';
+					else
+						message = count + ' stories were imported.';
+				}
+				else
+				{
+					className = 'alert-danger';
+					message = 'Sorry, no stories could be found in this file.';
+				}
+			}
+			catch (e)
+			{
+				className = 'alert-danger';
+				message = 'An error occurred while trying to import this file. (' + e.message + ')';
+			};
+
+			self.$('.stories').before('<div class="alert ' + className + 
+			                          '"><button class="close" data-dismiss="alert">&times;</button><p>' +
+									  message + '</p>');
+		};
+
+		reader.readAsText(e.target.files[0], 'UTF-8');
+		this.$('.importStory').popover('hide');
+	},
+
+	events:
+	{
+		'click .add': 'addStory',
+		'click .saveArchive': 'saveArchive',
+		'change .importFile': 'importFile'
+	}
+});
