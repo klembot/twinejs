b2328f74a24f32ba8f8bbaa55a050e0dd49f9c0b
Chris Klimas <chris@twinery.org>
2014-03-13 01:09:44 -0400

    Welcome view now pages through sections

--- js/views/welcomeview.js
+++ js/views/welcomeview.js
@@ -11,11 +11,21 @@ WelcomeView = Backbone.Marionette.ItemView.extend(
 
 	onRender: function()
 	{
-		this.$el.addClass('bounceIn');
+		this.$('div:first-child').css('display', 'block').addClass('bounceIn');
 
-		this.$el.on('click btn-default', function()
+		this.$el.on('click', 'button, a.done', function()
 		{
+			var $t = $(this);
+			var div = $t.closest('div');
+			var next = div.next('div');
 
+			div.fadeOut(400, function()
+			{
+				if ($t.hasClass('done'))
+					window.location.hash = '#stories';
+				else
+					next.fadeIn();
+			});
 		});
 	}
 });
