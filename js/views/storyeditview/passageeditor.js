927fc6f465b5baea12b1e003e74b6af18b412795
Chris Klimas <1405578+klembot@users.noreply.github.com>
2014-06-18 01:49:19 -0400

    First pass at UI for working with tags; kind of rough and needs documenting

--- js/views/storyeditview/passageeditor.js
+++ js/views/storyeditview/passageeditor.js
@@ -7,10 +7,27 @@
 
 StoryEditView.PassageEditor = Backbone.View.extend(
 {
+	tagTemplate:
+	'<span class="tag label label-info" data-name="<%- name %>"><%- name %><a href="javascript:void(0)" class="remove danger"><i class="fa fa-times"></i></a></span>',
+
 	initialize: function()
 	{
+		this.tagContainer = this.$('.tags');
+		this.tagTemplate = _.template(this.tagTemplate);
+
 		this.$el.modal({ show: false, backdrop: 'static' });
 		this.$el.on('hide.bs.modal', _.bind(this.save, this)); 
+		this.$el.on('click', '.showNewTag', _.bind(this.showNewTag, this));
+		this.$el.on('blur', '.newTag', _.bind(this.hideNewTag, this));
+		this.$el.on('submit', _.bind(function()
+		{
+			this.addTag(this.$('.newTag').val());	
+			this.hideNewTag();
+		}, this));
+		this.$el.on('click', '.tag .remove', function()
+		{
+			$(this).closest('.tag').remove();
+		});
 	},
 
 	/**
@@ -25,6 +42,12 @@ StoryEditView.PassageEditor = Backbone.View.extend(
 		this.$('.passageName').val(this.model.get('name'));
 		var text = this.model.get('text');
 		this.$('.passageText').val((text == Passage.prototype.defaults.text) ? '' : text);
+		
+		// sync tags
+
+		this.tagContainer.empty();
+		_.each(this.model.get('tags'), this.addTag, this);
+
 		this.$el.modal('show');
 	},
 
@@ -50,11 +73,21 @@ StoryEditView.PassageEditor = Backbone.View.extend(
 
 	save: function (e)
 	{
+		// gather current tag names
+
+		var tags = [];
+
+		this.$('.passageTags .tag').each(function()
+		{
+			tags.push($(this).attr('data-name'));
+		});
+
 		// try to save; we might error out if the passage name is a duplicate
 
 		if (this.model.save({
 			name: this.$('.passageName').val(),
-			text: this.$('.passageText').val()
+			text: this.$('.passageText').val(),
+			tags: tags
 		}))
 			this.$('.alert').remove();
 		else
@@ -76,5 +109,22 @@ StoryEditView.PassageEditor = Backbone.View.extend(
 			if (e)
 				e.stopImmediatePropagation();
 		};
+	},
+
+	showNewTag: function()
+	{
+		this.$('.showNewTag').hide();
+		this.$('.newTag').val('').show().focus();
+	},
+
+	hideNewTag: function()
+	{
+		this.$('.showNewTag').show();
+		this.$('.newTag').hide();
+	},
+
+	addTag: function (name)
+	{
+		this.tagContainer.append(this.tagTemplate({ name: name }));
 	}
 });
