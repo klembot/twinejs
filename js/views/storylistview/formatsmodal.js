9acecb4a1722e902498bf44964357d7881f4a160
Thomas Edwards <101626+tmedwards@users.noreply.github.com>
2014-12-25 06:45:51 -0600

    Fix for issue #83 ('Import fails to record story format').

---
+++ js/views/storylistview/formatsmodal.js
@@ -0,0 +1,250 @@
+'use strict';
+
+
+StoryListView.FormatsModal = Backbone.View.extend(
+{
+	initialize: function (options)
+	{
+		this.parent = options.parent;
+		this.itemTemplate = _.template($('.formatItem').html());
+	},
+
+	/**
+	 Opens a modal dialog for editing default formats.
+
+	 @method open
+	**/
+
+	open: function()
+	{
+		this.$('.error').hide();
+
+		// begin loading formats immediately
+
+		this.$('.storyFormatList, .proofingFormatList').empty();
+		this.formatsToLoad = StoryFormatCollection.all();
+		this.loadNextFormat();
+
+		this.$el.data('modal').trigger('show');
+	},
+
+	/**
+	 Closes the modal dialog for editing default formats.
+
+	 @method close
+	**/
+
+	close: function()
+	{
+		this.$el.data('modal').trigger('hide');
+	},
+
+	/**
+	 Incrementally loads information about each story format.
+ 	 If there are more remaining to be loaded, then this calls itself
+	 once the load is complete.
+
+	 @method loadNextFormat
+	**/
+
+	loadNextFormat: function()
+	{
+		if (this.formatsToLoad.length > 0)
+		{
+			var format = this.formatsToLoad.at(0);
+
+			format.load(_.bind(function()
+			{
+				// calculate containing directory for the format
+				// so that image URLs, for example, are correct
+
+				var path = format.get('url').replace(/\/[^\/]*?$/, '');
+				var fullContent = _.extend(format.properties, { path: path });
+				var content = $(this.itemTemplate(fullContent));
+
+				if (fullContent.proofing)
+					this.$('.proofingFormatList').append(content);
+				else
+					this.$('.storyFormatList').append(content);
+
+				this.formatsToLoad.remove(format);
+				this.loadNextFormat();
+			}, this));
+		}
+		else
+		{
+			this.syncButtons();
+			this.$('.loading').hide();
+		};
+	},
+
+	/**
+	 Tries to add a story format and update the list in the modal. If this succeeds,
+	 the tab where the format now belongs to is shown and the format description is
+	 animated in. If this fails, an error message is shown to the user. This call is
+	 asynchronous.
+
+	 @method addFormat
+	 @param {String} url URL of the new story format
+	**/
+
+	addFormat: function (url)
+	{
+		// create a temporary model and try loading it
+
+		var test = new StoryFormat({ url: url });	
+		this.$('.loading').fadeIn();
+
+		test.load(_.bind(function (err)
+		{
+			if (! err)
+			{
+				// save it for real
+
+				StoryFormatCollection.all().create({ name: test.properties.name, url: url });
+				
+				// add it to the appropriate list
+
+				var path = url.replace(/\/[^\/]*?$/, '');
+				var fullContent = _.extend(test.properties, { path: path });
+				var content = $(this.itemTemplate(fullContent));
+
+				if (fullContent.proofing)
+				{
+					this.$('.proofingFormatList').append(content);
+					this.$('.showProofingFormats').tab();
+					content.hide().slideDown();
+				}
+				else
+				{
+					this.$('.storyFormatList').append(content);
+					this.$('.showStoryFormats').tab();
+					content.hide().slideDown();
+				};
+
+				// clear the URL input
+
+				this.$('.addFormat input[type="text"]').val('');
+				this.$('.error').hide();
+			}
+			else
+				this.$('.error').fadeIn().html('The story format at ' + url +
+				      ' could not be added (' + err.message + ').');
+
+			this.$('.loading').hide();
+		}, this));
+	},
+
+	/**
+	 Removes a story format.
+
+	 @method removeFormat
+	 @param {String} name the name of the story format
+	**/
+
+	removeFormat: function (name)
+	{
+		StoryFormat.withName(name).destroy();
+	},
+
+	/**
+	 Sets the default story format.
+
+	 @method setDefaultFormat
+	 @param {String} name the name of the story format
+	**/
+
+	setDefaultFormat: function (name)
+	{
+		AppPref.withName('defaultFormat').save({ value: name });
+	},
+
+	/**
+	 Sets the default proofing format.
+
+	 @method setProofingFormat
+	 @param {String} name the name of the story format
+	**/
+
+	setProofingFormat: function (name)
+	{
+		AppPref.withName('proofingFormat').save({ value: name });
+	},
+
+	/**
+	 Syncs the active state of setDefault buttons with user preferences.
+
+	 @method syncButtons
+	**/
+
+	syncButtons: function()
+	{
+		var defaultFormat = AppPref.withName('defaultFormat').get('value');
+		var proofingFormat = AppPref.withName('proofingFormat').get('value');
+
+		this.$('.storyFormatList .format').each(function()
+		{
+			var $t = $(this);
+
+			if ($t.data('format') == defaultFormat)
+				$t.find('.setDefault').addClass('active');
+			else
+				$t.find('.setDefault').removeClass('active');
+		});
+
+		this.$('.proofingFormatList .format').each(function()
+		{
+			var $t = $(this);
+
+			if ($t.data('format') == proofingFormat)
+				$t.find('.setDefault').addClass('active');
+			else
+				$t.find('.setDefault').removeClass('active');
+		});
+	},
+
+	events:
+	{
+		'click .showRemoveConfirm': function (e)
+		{
+			var container = $(e.target).closest('.buttons');
+			container.find('.normalButtons').hide();
+			container.find('.removeConfirm').fadeIn();
+		},
+
+		'click .hideRemoveConfirm': function (e)
+		{
+			var container = $(e.target).closest('.buttons');
+			container.find('.normalButtons').fadeIn();
+			container.find('.removeConfirm').hide();
+		},
+
+		'click .remove': function (e)
+		{
+			var container = $(e.target).closest('.format');
+			this.removeFormat(container.data('format'));
+			container.slideUp();
+		},
+
+		'click .setDefault': function (e)
+		{
+			var container = $(e.target).closest('.format');
+			var format = container.data('format');
+			
+			if (container.closest('.storyFormatList').length > 0)
+				this.setDefaultFormat(format);
+			else if (container.closest('.proofingFormatList').length > 0)
+				this.setProofingFormat(format);
+			else
+				throw new Error("don't know what kind of format to set as default");
+
+			this.syncButtons();
+		},
+
+		'submit .addFormat': function (e)
+		{
+			this.addFormat(this.$('.addFormat input[type="text"]').val());
+			e.preventDefault();
+		}
+	}
+});
---
+++ js/views/storylistview/storagequota.js
@@ -0,0 +1,67 @@
+'use strict';
+
+StoryListView.StorageQuota = Backbone.View.extend(
+{
+	initialize: function (options)
+	{
+		this.parent = options.parent;
+		this.render();
+	},
+
+	render: function()
+	{
+		var usedEl = this.$('.used');
+		var percentEl = this.$('.percent');
+
+		// special case: we have no stories
+
+		if (this.parent.collection.length == 0)
+		{
+			usedEl.css('display', 'none');
+			percentEl.text('100');
+			return;
+		};
+
+		// otherwise, we test in 100k chunks 
+		
+		var used = JSON.stringify(window.localStorage).length;
+		var testChunk = new Array(102400).join('x');
+		var testString = testChunk;
+		var free = 102400;
+
+		var interval = window.setInterval(function()
+		{
+			var stop = false;
+
+			try
+			{
+				window.localStorage.setItem('__test', testString);
+				testString += testChunk;
+				free += 102400;
+
+				var percent = Math.round(used / (used + free) * 100);
+
+				percentEl.text(100 - percent);
+
+				if (percent <= 1)
+				{
+					usedEl.css('width', '0.25em');
+					percentEl.text('99');
+					stop = true;
+				}
+				else
+					usedEl.css('width', percent + '%');
+			}
+			catch (e)
+			{
+				stop = true;
+			};
+
+			if (stop)
+			{
+				window.localStorage.removeItem('__test');
+				window.clearInterval(interval);
+			};
+		}, 20);
+	}
+});
