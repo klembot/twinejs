1931370b6489581ff74ec3db26971de801eb6115
Chris Klimas <1405578+klembot@users.noreply.github.com>
2014-12-31 12:04:53 -0500

    Add automatic version check; runs daily against http://twinery.org/latestversion/2.json

--- js/models/apppref.js
+++ js/models/apppref.js
@@ -18,15 +18,26 @@ var AppPref = Backbone.Model.extend(
 });
 
 /**
- Locates an AppPref by name. If none exists, then this returns null.
+ Locates an AppPref by name. If none exists and a default value is
+ specified, then a new model is saved with the default and returned.
+ If not, then this returns null.
 
  @method withName
  @param {String} name name of the preference
+ @param defaultVal default value, optional
  @static
  @return {AppPref} pref with matching name
- **/
+**/
 
-AppPref.withName = function (name)
+AppPref.withName = function (name, defaultVal)
 {
-	return AppPrefCollection.all().findWhere({ name: name });
+	var allPrefs = AppPrefCollection.all();
+	var result = allPrefs.findWhere({ name: name });
+
+	if (result)
+		return result;
+	else if (defaultVal !== null)
+		return allPrefs.create({ name: name, value: defaultVal })
+	else
+		return;
 };
