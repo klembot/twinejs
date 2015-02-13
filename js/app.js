fe546d3065c6cfbacbdea0593bd06da185a28034
Chris Klimas <chris@twinery.org>
2015-02-13 00:36:56 -0500

    Bump version

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ var TwineApp = Backbone.Marionette.Application.extend(
 	 @property version
 	**/
 
-	version: '2.0.2',
+	version: '2.0.3',
 
 	/**
 	 Saves data to a file. This appears to the user as if they had clicked
