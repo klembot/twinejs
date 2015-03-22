5f398e5a33e40e7fa1cc425f3dfe0ed31eba2ec1
Leon <83939+webbedspace@users.noreply.github.com>
2015-03-22 22:56:08 +1000

    Story format passage syntax highlighting support.
    This extends the format.js format, adding a "lexer" property which consists of a stringified function. This function is capable of defining a CodeMirror mode for passages in that story format. (However, it's also intended for providing future features related to lexing, like early error diagnostics, or special passage arrow/tag colouring.)
    
    This also extends PassageEditView to support using that CodeMirror mode for the passage editor pane if it exists, and defaulting to text if it does not.

---
+++ README.md
@@ -0,0 +1,44 @@
+twinejs
+-------
+
+by Chris Klimas, Leon Arnott, Daithi O Crualaoich, Ingrid Cheung, Thomas
+Michael Edwards, Micah Fitch, Juhana Leinonen, and Ross Smith
+
+### SYNOPSIS
+
+This is a port of Twine to a local browser-based app. See
+[twinery.org](http://twinery.org) for more info.
+
+The story formats in minified format under storyformats/ exist in separate
+repositories:
+* [Harlowe](https://bitbucket.org/_L_/harlowe)
+* [Snowman](https://bitbucket.org/klembot/snowman-2)
+* [Paperthin](https://bitbucket.org/klembot/paperthin)
+
+### INSTALL
+
+Run `npm install` at the top level of the directory to install all goodies.
+
+You need [Grunt](http://gruntjs.com) to continue. Run `npm install -g grunt-cli`
+(you need to have root privileges to achieve this task)
+
+### BUILDING
+
+Run `grunt` to perform a basic build, including creating documentation in doc/;
+`grunt watch` will perform the same tasks whenever you make changes to the
+source code. `grunt release` will minify everything to as few files as possible
+into dist/html/.
+
+To build executable versions of Twine with [NW.js](http://nwjs.io/), run `grunt
+release-nw`, which will publish the files to dist/apps/. In order to build
+Windows apps on OS X or Linux, you will need to have
+[Wine](https://www.winehq.org/) installed. **nb. NW.js builds are currently
+considered experimental, and have not been well-tested or documented.**
+
+### TESTING
+
+This uses [Selenium IDE](http://docs.seleniumhq.org/projects/ide/) for
+automated browser testing. Unfortunately, Selenium IDE does not like running on
+the file:// protocol because of JavaScript security restrictions. To facilitate
+testing, run `grunt server`, which will spin up a basic web server on port
+8000.
---
+++ js/views/storyeditview/passageeditor.js
@@ -0,0 +1,228 @@
+/**
+ Manages the passage editor modal of a StoryEditView.
+
+ @class StoryEditView.PassageEditor
+ @extends Backbone.View
+**/
+
+'use strict';
+
+StoryEditView.PassageEditor = Backbone.View.extend(
+{
+	tagTemplate:
+	'<span class="tag label label-info" data-name="<%- name %>"><%- name %><button class="remove"><i class="fa fa-times"></i></button></span>',
+
+	initialize: function(options)
+	{
+		this.tagContainer = this.$('.tags');
+		this.tagTemplate = _.template(this.tagTemplate);
+		// Required to identify the current story format
+		this.story = options.parent.model;
+
+		this.passageEditor = CodeMirror.fromTextArea(this.$('.passageText')[0],
+		{
+			lineWrapping: true,
+			lineNumbers: false,
+			mode: 'text',
+		});
+
+		this.$el.on('modalhide', _.bind(this.restoreTitle, this)); 
+		this.$el.on('click', '.showNewTag', _.bind(this.showNewTag, this));
+		this.$el.on('click', '.hideNewTag', _.bind(this.hideNewTag, this));
+		this.$el.on('submit', _.bind(function (e)
+		{
+			var name = this.$('.newTagName').val().replace(/\s/g, '-');
+
+			// don't add duplicate tags
+
+			if (this.model.get('tags').indexOf(name) == -1)
+				this.addTag(name);
+
+			this.hideNewTag();
+			e.preventDefault();
+		}, this));
+
+		this.$el.on('click', '.tag .remove', function()
+		{
+			$(this).closest('.tag').remove();
+		});
+
+		this.$el.data('blockModalHide', _.bind(function()
+		{
+			var worked = this.save();
+
+			if (worked)
+				window.onbeforeunload = null;
+			
+			return ! worked;
+		}, this));
+	},
+
+	/**
+	 Opens a modal dialog for editing a passage.
+
+	 @method open
+	**/
+
+	open: function(options)
+	{
+		// remember previous window title
+
+		this.prevTitle = document.title;
+		document.title = 'Editing \u201c' + this.model.get('name') + '\u201d';
+
+		this.$('.passageId').val(this.model.id);
+		this.$('.passageName').val(this.model.get('name'));
+
+		/*
+			Load the story format, which may install a CodeMirror mode named after itself.
+			We use that mode if it is found to exist after loading.
+		*/
+		var storyFormatName = this.story.get('storyFormat');
+		StoryFormat.withName(storyFormatName).load(function(err)
+		{
+			var modeName = storyFormatName.toLowerCase();
+
+			if (!err && modeName in CodeMirror.modes)
+			{
+				/*
+					This is a small hack to allow modes such as Harlowe to access the
+					full text of the textarea, permitting its lexer to grow
+					a syntax tree by itself.
+				*/
+				CodeMirror.modes[modeName].doc = this.passageEditor.doc;
+				/*
+					Now that's done, we can assign the mode and trigger a re-render.
+				*/
+				this.passageEditor.setOption('mode', modeName);
+			}
+			else
+			{
+				this.passageEditor.setOption('mode', 'text');
+			}
+		}.bind(this));
+
+		var text = this.model.get('text');
+		this.passageEditor.doc.setValue((text == Passage.prototype.defaults.text) ? '' : text);
+
+		// sync tags
+
+		this.tagContainer.empty();
+		_.each(this.model.get('tags'), this.addTag, this);
+
+		this.$el.data('modal').trigger('show');
+
+		// warn the user about leaving before saving
+
+		window.onbeforeunload = function()
+		{
+			return 'Any changes to the passage you\'re editing haven\'t been saved yet. (To do so, close the passage editor.)';
+		};
+	},
+
+	/**
+	 Closes the modal dialog for editing.
+
+	 @method close
+	**/
+
+	close: function()
+	{
+		this.$el.data('modal').trigger('hide');
+	},
+
+	/**
+	 Saves changes made by the user to the model, displaying any validation
+	 errors.
+
+	 @method save
+	 @param {Event} e Event to stop
+	 @return {Boolean} whether the save was successful
+	**/
+
+	save: function ()
+	{
+		// gather current tag names
+
+		var tags = [];
+
+		this.$('.passageTags .tag').each(function()
+		{
+			tags.push($(this).attr('data-name'));
+		});
+
+		// try to save; we might error out if the passage name is a duplicate
+
+		if (this.model.save({
+			name: this.$('.passageName').val(),
+			text: this.passageEditor.doc.getValue(),
+			tags: tags
+		}))
+		{
+			// have to manually set the style here because of jQuery .fadeIn()
+
+			this.$('.error').addClass('hide').hide();
+			this.$el.removeClass('hasError');
+			return true;
+		}
+		else
+		{
+			// show the error message
+
+			var message = this.$('.error');
+			message.removeClass('hide').text(this.model.validationError).hide().fadeIn();
+			this.$el.addClass('hasError');
+			this.$('.passageName').focus();
+			return false;
+		};
+	},
+
+	/**
+	 Shows the UI for adding a new tag.
+
+	 @method showNewTag
+	**/
+
+	showNewTag: function()
+	{
+		this.$('.showNewTag').hide();
+		this.$('.newTag').show();
+		this.$('.newTagName').val('').focus();
+	},
+
+	/**
+	 Hides the UI for adding a new tag.
+
+	 @method showNewTag
+	**/
+
+	hideNewTag: function()
+	{
+		this.$('.showNewTag').show();
+		this.$('.newTag').hide();
+	},
+
+	/**
+	 Adds a new tag to the list. This does not affect the model
+	 at all and thus has no validation associated with it.
+
+	 @method addTag
+	 @param {String} name name of the tag to add
+	**/
+
+	addTag: function (name)
+	{
+		this.tagContainer.append(this.tagTemplate({ name: name }));
+	},
+
+	/**
+	 Restores the window title after finishing editing.
+
+	 @method restoreTitle
+	**/
+
+	restoreTitle: function()
+	{
+		document.title = this.prevTitle;
+	}
+});
