3655d8cc4afd4b4c953adede16ba0dafd4883e2c
Chris Klimas <1405578+klembot@users.noreply.github.com>
2015-04-08 22:39:38 -0400

    Show last save date as a tooltip of the story menu itself

--- js/views/storyeditview/toolbar.js
+++ js/views/storyeditview/toolbar.js
@@ -13,8 +13,11 @@ StoryEditView.Toolbar = Backbone.View.extend(
 	{
 		this.parent = options.parent;
 		this.syncZoomButtons();
-		this.parent.model.on('change:zoom', this.syncZoomButtons, this);
-		this.parent.model.on('change:name', this.syncStoryName, this);
+		this.syncStorySaved();
+		this.listenTo(this.parent.model, 'change:zoom', this.syncZoomButtons);
+		this.listenTo(this.parent.model, 'change:name', this.syncStoryName);
+		this.listenTo(this.parent.model, 'update', this.syncStorySaved);
+		this.listenTo(this.parent.collection, 'update', this.syncStorySaved);
 	},
 
 	/**
@@ -71,6 +74,23 @@ StoryEditView.Toolbar = Backbone.View.extend(
 			menu.addClass('checked');
 		else
 			menu.removeClass('checked');
+	},
+
+	/**
+	 Sets the tooltip of the story menu to indicate that a save has
+	 just occurred.
+
+	 @method syncStorySaved
+	 @param {Date} forceDate If passed, uses this date instead of the current one
+	**/
+
+	syncStorySaved: function (forceDate)
+	{
+		var $sn = this.$('.storyName');
+		var date = (forceDate) ? new XDate(forceDate) : new XDate();
+		
+		$sn.attr('title', $sn.data('datetext').replace('%', date.toString($sn.data('dateformat'))));
+		$sn.powerTip();
 	},
 
 	events:
