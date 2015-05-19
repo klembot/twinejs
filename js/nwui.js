3096d36e8bc8117e867d924a390b485aae1db3b2
Chris Klimas <chris@twinery.org>
2015-05-18 21:47:34 -0400

    Make sure story file names are legal
    Resolves #139

--- js/nwui.js
+++ js/nwui.js
@@ -344,6 +344,21 @@ var nwui =
 	},
 
 	/**
+	 Returns a filename for a story model that's guaranteed to be
+	 safe across all platforms. For this, we use POSIX's definition
+	 (http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_276)
+	 with the addition of spaces, for legibility.
+
+	 @method storyFileName
+	 @param {Story} story Story model to create filename for
+	**/
+
+	storyFileName: function (story)
+	{
+		return story.get('name').replace(/[^\w\. -]/g, '_') + '.html';
+	},
+
+	/**
 	 Saves a story model to the file system. If a problem occurs,
 	 then a notification is shown to the user.
 
@@ -356,7 +371,7 @@ var nwui =
 		try
 		{
 			nwui.unlockStoryDirectory();
-			var fd = nwui.fs.openSync(nwui.filePath + story.get('name') + '.html', 'w');
+			var fd = nwui.fs.openSync(nwui.filePath + nwui.storyFileName(story), 'w');
 			nwui.fs.writeSync(fd, story.publish());
 			nwui.fs.closeSync(fd);
 		}
@@ -384,7 +399,7 @@ var nwui =
 		try
 		{
 			nwui.unlockStoryDirectory();
-			nwui.fs.unlinkSync(nwui.filePath + story.get('name') + '.html');
+			nwui.fs.unlinkSync(nwui.filePath + nwui.storyFileName(story));
 		}
 		catch (e)
 		{
