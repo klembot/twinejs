ebe278de9bb6e6cb71295eed9ede2063ccf41fa2
Chris Klimas <1405578+klembot@users.noreply.github.com>
2015-08-30 08:53:13 -0400

    Always use local version of SugarCube

--- src/common/app.js
+++ src/common/app.js
@@ -117,12 +117,7 @@ var TwineApp = module.exports = Marionette.Application.extend(
 			formats.create({ name: 'Paperthin', url: 'storyformats/Paperthin/format.js', userAdded: false });
 
 		if (! formats.findWhere({ name: 'SugarCube' }))
-		{
-			if (nwui.active)
-				formats.create({ name: 'SugarCube', url: 'storyformats/SugarCube/format.js', userAdded: false });
-			else
-				formats.create({ name: 'SugarCube', url: 'http://www.motoslave.net/sugarcube/1/twine2/format.js', userAdded: false });
-		};
+			formats.create({ name: 'SugarCube', url: 'storyformats/SugarCube/format.js', userAdded: false });
 
 		// set default formats if not already set
 		// (second param is a default)
