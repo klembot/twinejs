47191fc40b7f8744f718f518a32ebaaf21bbb9b2
Thomas Edwards <101626+tmedwards@users.noreply.github.com>
2015-11-21 15:11:00 -0600

    Added a few more setter link unit tests for `Passage.links()`.

--- tests/unit/passage.js
+++ tests/unit/passage.js
@@ -71,9 +71,49 @@ describe('Passage', function()
 			assert(links.length == 1 && links[0] == 'link');
 		});
 
+		it('parses [[simple links][]] while ignoring empty setter component', function()
+		{
+			p.set('text', '[[link][]]');
+			var links = p.links();
+			assert(links.length == 1 && links[0] == 'link');
+		});
+
+		it('parses [[pipe|links][]] while ignoring empty setter component', function()
+		{
+			p.set('text', '[[display|link][]]');
+			var links = p.links();
+			assert(links.length == 1 && links[0] == 'link');
+		});
+
+		it('parses [[arrow->links][]] while ignoring empty setter component', function()
+		{
+			p.set('text', '[[display->link][]]');
+			var links = p.links();
+			assert(links.length == 1 && links[0] == 'link');
+		});
+
+		it('parses [[backarrow<-links][]] while ignoring empty setter component', function()
+		{
+			p.set('text', '[[link<-display][]]');
+			var links = p.links();
+			assert(links.length == 1 && links[0] == 'link');
+		});
+
 		it('ignores [[]]', function()
 		{
 			p.set('text', '[[]]');
+			assert(p.links().length == 0);
+		});
+
+		it('ignores [[][]]', function()
+		{
+			p.set('text', '[[][]]');
+			assert(p.links().length == 0);
+		});
+
+		it('ignores [[][setter]]', function()
+		{
+			p.set('text', '[[][setter]]');
 			assert(p.links().length == 0);
 		});
 	});
