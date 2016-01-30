7c44390c87afec3513521a29a9c6292a2e500eba
Chris Klimas <1405578+klembot@users.noreply.github.com>
2016-01-30 12:05:38 -0500

    Remove outdated app.html, fix grunt module reference

--- grunt/build.js
+++ grunt/build.js
@@ -191,7 +191,7 @@ module.exports = function (grunt)
 				{
 					data: _.extend(
 					{
-						buildNumber: require('./buildNumber')(),
+						buildNumber: require('./build-number')(),
 						cdn: false
 					}, twine)
 				}
@@ -206,7 +206,7 @@ module.exports = function (grunt)
 				{
 					data: _.extend(
 					{
-						buildNumber: require('./buildNumber')(),
+						buildNumber: require('./build-number')(),
 						cdn: true
 					}, twine)
 				}
