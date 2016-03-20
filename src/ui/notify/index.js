23fd4551ac6c549e835ae1be14a72ea0d7bf0df1
Chris Klimas <chris@twinery.org>
2016-03-20 19:53:28 -0400

    Vueify UI notifications

---
+++ src/ui/notify/index.js
@@ -0,0 +1,14 @@
+const NotificationList = require('./list');
+let notifications;
+
+module.exports = function(html, className = '') {
+	if (!notifications) {
+		var container = document.createElement('div');
+		document.querySelector('body').appendChild(container);
+		notifications = new NotificationList({ el: container });
+	}
+
+	notifications.add(html, className);
+};
+
+window.notify = module.exports;
