468f6a7c60b7a7d39ac89d8fa225cce9528f28ec
Chris Klimas <chris@twinery.org>
2016-05-29 18:05:52 -0400

    Progress on story item previews

--- src/story-list-view/story-item/item-preview/index.js
+++ src/story-list-view/story-item/item-preview/index.js
@@ -8,20 +8,28 @@ const _ = require('underscore');
 const Vue = require('vue');
 const SVG = require('svg.js');
 
-/**
- How fast we animate passages appearing, in milliseconds.
- @property appearDuration
- @static
-**/
-
-const appearDuration = 500;
-
 module.exports = Vue.extend({
-	template: '<div class="preview"></div>',
+	template: require('./index.html'),
 
 	props: {
 		hue: Number,
 		passages: Array,
+	},
+
+	computed: {
+		style() {
+			return {
+				background: `hsl(${this.hue}, 85%, 95%)`
+			};
+		},
+
+		passageColor() {
+			if (this.hue < 240) {
+				return `hsla(${this.hue + 120}, 90%, 50%, 0.6)`;
+			}
+
+			return `hsla(${this.hue - 120}, 90%, 50%, 0.6)`;
+		}
 	},
 
 	ready() {
@@ -42,10 +50,6 @@ module.exports = Vue.extend({
 
 			// render passages
 
-			const c1 = 'hsl(' + this.hue + ', 88%, 40%)';
-			const c2 = 'hsl(' + ((this.hue - 30) % 360) + ', 88%, 40%)';
-			const c3 = 'hsl(' + ((this.hue + 30) % 360) + ', 88%, 40%)';
-
 			let minX = Number.POSITIVE_INFINITY;
 			let minY = Number.POSITIVE_INFINITY;
 			let maxX = Number.NEGATIVE_INFINITY;
@@ -53,40 +57,25 @@ module.exports = Vue.extend({
 
 			_.each(this.passages, (passage, i) => {
 				const ratio = passage.get('text').length / maxLength;
-				const size = 100 + 200 * ratio;
-				const x = passage.get('left');
-				const y = passage.get('top');
-				const c = svg.circle().center(x + 50, y + 50);
+				const radius = (300 + 300 * ratio) / 2;
+				const x = passage.get('left') + 50;
+				const y = passage.get('top') + 50;
+				const c = svg.ellipse()
+					.center(x, y)
+					.radius(radius, radius)
+					.rotate(15 + Math.random() * 15)
+					.fill(this.passageColor);
 
-				if (i % 3 === 0) {
-					c.fill({ color: c1, opacity: ratio * 0.9 });
-				}
-				else {
-					if (i % 2 === 0) {
-						c.fill({ color: c2, opacity: ratio * 0.9 });
-					}
-					else {
-						c.fill({ color: c3, opacity: ratio * 0.9 });
-					}
-				}
+				if (x - radius < minX) { minX = x - radius; }
 
-				c.animate(appearDuration, '>').radius(size / 2);
+				if (x + radius > maxX) { maxX = x + radius; }
 
-				if (x - size < minX) { minX = x - size; }
+				if (y - radius < minY) { minY = y - radius; }
 
-				if (x + size > maxX) { maxX = x + size; }
-
-				if (y - size < minY) { minY = y - size; }
-
-				if (y + size > maxY) { maxY = y + size; }
+				if (y + radius > maxY) { maxY = y + radius; }
 			});
 
-			svg.viewbox(
-				minX,
-				minY,
-				Math.abs(minX) + maxX,
-				Math.abs(minY) + maxY
-			);
+			svg.viewbox(minX, minY, maxX - minX, maxY - minY);
 		}
 		else {
 			// special case single or no passage
@@ -95,8 +84,7 @@ module.exports = Vue.extend({
 				svg
 					.circle()
 					.center(5, 5)
-					.fill('hsl(' + this.hue + ', 88%, 40%)')
-					.animate(appearDuration, '>')
+					.fill(this.passageColor)
 					.radius(2.5);
 				svg.viewbox(0, 0, 10, 10);
 			}
