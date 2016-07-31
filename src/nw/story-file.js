a0f57e5a11363e3016e00393fb726f9071c15f56
Chris Klimas <chris@twinery.org>
2016-07-31 01:05:58 -0400

    NW.js build working on a basic level
    Seems to save changes correctly, needs testing

--- src/nw/story-file.js
+++ src/nw/story-file.js
@@ -1,28 +1,37 @@
-// Manages interactions with individual story files.
+/*
+Manages interactions with individual story files.
+*/
 
 const storyFile = module.exports = {
-	// A global flag that allows deactivation of the module. This is needed
-	// while the module itself works, so that changes in local storage made
-	// here don't trigger a save to the filesystem, which in turn would trigger
-	// a change to local storage, etc.
+	/*
+	A global flag that allows deactivation of the module. This is needed while
+	the module itself works, so that changes in local storage made here don't
+	trigger a save to the filesystem, which in turn would trigger a change to
+	local storage, etc.
+	*/
 
 	active: true,
 
-	// Returns a filename for a story model that's guaranteed to be safe across
-	// all platforms. For this, we use POSIX's definition
-	// (http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_276)
-	// with the addition of spaces, for legibility.
+	/*
+	Returns a filename for a story object that's guaranteed to be safe across
+	all platforms. For this, we use POSIX's definition
+	(http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_276)
+	with the addition of spaces, for legibility.
+	*/
 
 	fileName(story) {
-		return story.get('name').replace(/[^\w\. -]/g, '_') + '.html';
+		return story.name.replace(/[^\w\. -]/g, '_') + '.html';
 	},
 
-	// Saves a story model to the file system.
+	/*
+	Saves a story object to the file system.
+	*/
 
 	save(story) {
 		const fs = require('fs');
 		const directories = require('./directories');
 		const path = require('path');
+		const { publishStory } = require('../data/publish');
 
 		try {
 			directories.unlockStories();
@@ -31,7 +40,7 @@ const storyFile = module.exports = {
 				'w'
 			);
 
-			fs.writeSync(fd, story.publish(null, null, true));
+			fs.writeSync(fd, publishStory(story, null, null, true));
 			fs.closeSync(fd);
 		}
 		finally {
@@ -39,7 +48,9 @@ const storyFile = module.exports = {
 		}
 	},
 
-	// Deletes a story file from the file system.
+	/*
+	Deletes a story file from the file system.
+	*/
 
 	delete(story) {
 		const fs = require('fs');
@@ -49,7 +60,7 @@ const storyFile = module.exports = {
 		try {
 			directories.unlockStories();
 			fs.unlinkSync(
-				path.join(directories.storyPath(), storyFile.fileName(story))
+				path.join(directories.storiesPath(), storyFile.fileName(story))
 			);
 		}
 		finally {
@@ -57,65 +68,56 @@ const storyFile = module.exports = {
 		}
 	},
 
-	// Syncs local storage with the file system, obliterating any stories that
-	// happen to be saved to local storage only.
+	/*
+	Syncs local storage with the file system, obliterating any stories that
+	happen to be saved to local storage only. This is run at startup time, so
+	efficiency is important.
+	*/
 
 	loadAll() {
-		//FIXME
-		//const StoryCollection = require('../data/collections/story');
 		const directories = require('./directories');
 		const fs = require('fs');
-		//const fileImport = require('../file/import');
 		const path = require('path');
+		const { deleteStory, importStory } = require('../data/actions');
+		const importFile = require('../data/import');
+		const store = require('../data/store');
 
 		storyFile.active = false;
 
 		try {
-			// Clear all existing stories.
+			/*
+			Delete all existing stories. We save the IDs in a separate step so
+			that as we delete stories, the source list isn't affected.
+			*/
 
-			const allStories = StoryCollection.all();
+			const storyIds = store.state.story.stories.map(story => story.id);
+			storyIds.forEach(id => deleteStory(store, id));
 
-			while (allStories.length > 0) {
-				allStories.at(0).destroy();
-			}
-
-			// Iterate over all files in the stories directory.
+			/*
+			Generate import actions for each .html file in the stories
+			directory.  
+			*/
 
 			directories.lockStories();
 
 			const storyPath = directories.storiesPath();
 			const fileStories = fs.readdirSync(storyPath);
 
-			// obtain the full contents of every file, sync
-
 			fileStories.map(filename => {
 				if (filename.match(/\.html$/)) {
 					const filePath = path.join(storyPath, filename);
+					const source = fs.readFileSync(
+						filePath,
+						{ encoding: 'utf8' }
+					);
 					const stats = fs.statSync(filePath);
 
-					return {
-						file: fs.readFileSync(filePath, { encoding: 'utf-8' }),
-						stats
-					};
+					importStory(
+						store,
+						importFile(source, new Date(Date.parse(stats.mtime)))[0]
+					);
 				}
 			})
-
-			// remove undefineds from the array
-
-			.filter(Boolean)
-
-			// import the files. It doesn't matter if this is async,
-			// because all of the filesystem I/O is finished.
-
-			.reduce(
-				(promise, {file, stats}) => promise.then(()=>
-				fileImport.importData(file, {
-					lastUpdate: new Date(Date.parse(stats.mtime)),
-					confirmReplace: false,
-					silent: true
-				})),
-				Promise.resolve()
-			);
 		}
 		finally {
 			directories.unlockStories();
