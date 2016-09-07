9abc6819c4da3fb2184a508e43d4dfdd12adbe72
Chris Klimas <1405578+klembot@users.noreply.github.com>
2016-09-06 23:00:31 -0400

    Fix lookup of story format

--- src/common/router.js
+++ src/common/router.js
@@ -67,7 +67,8 @@ TwineRouter.map({
 				const story = state.story.stories.find(
 					story => story.id === this.$route.params.id
 				);
-				const formatName = story.format || state.pref.defaultFormat;
+
+				const formatName = story.storyFormat || state.pref.defaultFormat;
 				const format = state.storyFormat.formats.find(
 					format => format.name === formatName
 				);
@@ -112,7 +113,7 @@ TwineRouter.map({
 				const story = state.story.stories.find(
 					story => story.id === this.$route.params.id
 				);
-				const formatName = story.format || state.pref.defaultFormat;
+				const formatName = story.storyFormat || state.pref.defaultFormat;
 				const format = state.storyFormat.formats.find(
 					format => format.name === formatName
 				);
@@ -136,7 +137,7 @@ TwineRouter.map({
 				const story = state.story.stories.find(
 					story => story.id === this.$route.params.storyId
 				);
-				const formatName = story.format || state.pref.defaultFormat;
+				const formatName = story.storyFormat || state.pref.defaultFormat;
 				const format = state.storyFormat.formats.find(
 					format => format.name === formatName
 				);
