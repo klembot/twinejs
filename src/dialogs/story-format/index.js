d3abdcc4b185a1253a7cc4ecccff6eeed3604a96
Chris Klimas <1405578+klembot@users.noreply.github.com>
2016-11-07 20:14:18 -0500

    Revise story format picker to accomodate multiple versions

--- src/dialogs/story-format/index.js
+++ src/dialogs/story-format/index.js
@@ -2,6 +2,7 @@
 
 const Vue = require('vue');
 const { loadFormat } = require('../../data/actions');
+const semverUtils = require('semver-utils');
 
 module.exports = Vue.extend({
 	template: require('./index.html'),
@@ -9,7 +10,7 @@ module.exports = Vue.extend({
 	data: () => ({
 		loadIndex: 0,
 		loadedFormats: [],
-		storyId: ''
+		storyId: '',
 	}),
 
 	computed: {
@@ -17,8 +18,15 @@ module.exports = Vue.extend({
 			return this.allStories.find(story => story.id === this.storyId);
 		},
 
+		selectedFormat() {
+			return this.loadedFormats.find(
+				format => format.name === this.story.storyFormat &&
+					format.version === this.story.storyFormatVersion
+			);
+		},
+
 		working() {
-			return this.loadIndex < this.formatNames.length;
+			return this.loadIndex < this.allFormats.length;
 		}
 	},
 
@@ -28,7 +36,9 @@ module.exports = Vue.extend({
 				return;
 			}
 
-			this.loadFormat(this.formatNames[this.loadIndex])
+			const nextFormat = this.allFormats[this.loadIndex];
+
+			this.loadFormat(nextFormat.name, nextFormat.version)
 			.then(format => {
 				if (!format.properties.proofing) {
 					this.loadedFormats.push(format);
@@ -51,13 +61,57 @@ module.exports = Vue.extend({
 
 		getters: {
 			allStories: state => state.story.stories,
-			formatNames: state => state.storyFormat.formats.map(
-				format => format.name
-			)
+			allFormats: state => {
+				var result = state.storyFormat.formats.map(
+					format => ({ name: format.name, version: format.version })
+				);
+				
+				result.sort((a, b) => {
+					if (a.name < b.name) {
+						return -1;
+					}
+					
+					if (a.name > b.name) {
+						return 1;
+					}
+
+					const aVersion = semverUtils.parse(a.version);
+					const bVersion = semverUtils.parse(b.version);
+
+					if (aVersion.major > bVersion.major) {
+						return -1;
+					}
+					else if (aVersion.major < bVersion.major) {
+						return 1;
+					}
+					else {
+						if (aVersion.minor > bVersion.minor) {
+							return -1;
+						}
+						else if (aVersion.minor < bVersion.minor) {
+							return 1;
+						}
+						else {
+							if (aVersion.patch > bVersion.patch) {
+								return -1;
+							}
+							else if (aVersion.patch < bVersion.patch) {
+								return 1;
+							}
+							else {
+								return 0;
+							}
+						}
+					}
+				});
+
+				return result;
+			}
 		}
 	},
 
 	components: {
+		'format-detail': require('./detail'),
 		'format-item': require('./item'),
 		'modal-dialog': require('../../ui/modal-dialog')
 	}
