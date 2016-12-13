b72d24a06540b4a180e92cb5b2668bd900f57762
Valentin Rocher <bishiboosh@gmail.com>
2016-12-13 09:02:20 +0100

    merge from klembot/twinejs

--- src/dialogs/app-update/index.js
+++ src/dialogs/app-update/index.js
@@ -33,8 +33,10 @@ module.exports = {
 
 					confirm({
 						message:
-							// L10n: The <span> will have a version number, i.e.
-							// 2.0.6, interpolated into it.
+							/*
+							L10n: The <span> will have a version number, i.e.
+							2.0.6, interpolated into it.
+							*/
 							locale.say('A new version of Twine, <span class="version"></span>, has been released.').replace('><', '>' + version + '<'),
 
 						buttonLabel:
@@ -42,8 +44,10 @@ module.exports = {
 							locale.say('Download'),
 
 						cancelLabel:
-							// L10n: A polite rejection of a request, in the sense that the answer
-							// may change in the future.
+							/*
+							L10n: A polite rejection of a request, in the sense that the answer
+							may change in the future.
+							*/
 							locale.say('Not Right Now'),
 
 						buttonClass:
