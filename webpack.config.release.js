f03a07cda625c49b5b1c8bf71ad72359dc0612f8
Chris Klimas <1405578+klembot@users.noreply.github.com>
2017-01-24 22:07:53 -0500

    Fix bug with disappearing text in NW.js
    Easiest way to repro it was to play a story in another window, then continue
    editing in the main window. See https://github.com/nwjs/nw.js/issues/5080 for
    more details.

--- webpack.config.release.js
+++ webpack.config.release.js
@@ -15,12 +15,31 @@ config.module.loaders.push({
 	}
 });
 
+/*
+Base64 encode all fonts to work around a bug in NW.js -- see
+https://github.com/nwjs/nw.js/issues/5080
+*/
+
+config.module.loaders.splice(0, 1);
+
+config.module.loaders.push({
+	test: /\.(png|svg)(\?.*)?$/,
+	loader: 'url-loader?limit=10000&name=rsrc/[name].[hash].[ext]'
+});
+
+config.module.loaders.push({
+	test: /\.(woff|woff2|ttf|eot|svg)(\?.*)?$/,
+	exclude: /img/,
+	loader: 'base64-font-loader'
+});
+
 /* Minify the JS. */
 
 config.plugins.push(new UglifyPlugin({ minimize: true }));
 
 /* Minify the CSS. */
 
-config.lessLoader.lessPlugins.push(new CleanPlugin());
+//config.lessLoader.lessPlugins.push(new CleanPlugin());
+
 
 module.exports = config;
