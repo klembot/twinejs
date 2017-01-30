fefef64cb3ea686577698b72d34621af920ef05a
Chris Klimas <1405578+klembot@users.noreply.github.com>
2017-01-29 22:35:45 -0500

    Allow building NW.js versions for a single platform (to make testing faster)

--- scripts/build-nw.js
+++ scripts/build-nw.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const fsExtra = require('fs-extra');
 const NwBuilder = require('nw-builder');
 
+var argv = require('yargs').argv;
+
+var platforms = argv.platform ? [argv.platform] :
+	['osx64', 'win32', 'win64', 'linux32', 'linux64'];
+
 fsExtra.copySync('package.json', 'dist/web/package.json');
 
 var nw = new NwBuilder({
 	files: 'dist/web/**',
-	platforms: ['osx64', 'win32', 'win64', 'linux32', 'linux64'],
+	platforms: platforms,
 	version: '0.19.5',
 	buildDir: 'dist/nw',
 	cacheDir: 'nw-cache/',
