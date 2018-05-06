8aa23fadf1289c4514e857a696d7ead35615722b
cpb8010 <cpb8010@gmail.com>
2018-05-06 17:03:58 -0500

    73. Replace filtered value in v-html="'If you've never used Twine before, then welcome! The Twine 2 Guide and the official wiki in general, are a great place to learn. Keep in mind that some articles on the wiki at larger were written for Twine 1, which is a little bit different than this version. But most of the concepts are the same.' | say" with a method or computed property   Line 25: src/welcome/index.html   Reason: Filters can now only be used inside text interpolations, e.g. {{ date | formatDate('YY-MM-DD') }}   More info: http://vuejs.org/guide/migration.html#Filters-Outside-Text-Interpolations
    74. Replace filtered value in v-html="'You can also get help over at the Twine forum, too.' | say" with a method or computed property
      Line 27: src/welcome/index.html
      Reason: Filters can now only be used inside text interpolations, e.g. {{ date | formatDate('YY-MM-DD') }}
      More info: http://vuejs.org/guide/migration.html#Filters-Outside-Text-Interpolations
    
    75. Replace filtered value in v-html="'If you have used Twine 1 before, the guide also has details on what has changed in this version. Chief among them is a new default story format, Harlowe. But if you find you prefer the Twine 1 scripting syntax, try using SugarCube instead.' | say" with a method or computed property
      Line 29: src/welcome/index.html
      Reason: Filters can now only be used inside text interpolations, e.g. {{ date | formatDate('YY-MM-DD') }}
      More info: http://vuejs.org/guide/migration.html#Filters-Outside-Text-Interpolations
    
    76. Replace filtered value in v-html="'That means you don't need to create an account to use Twine 2, and everything you create isn't stored on a server somewhere else &mdash; it stays right in your browser.' | say" with a method or computed property
      Line 43: src/welcome/index.html
      Reason: Filters can now only be used inside text interpolations, e.g. {{ date | formatDate('YY-MM-DD') }}
      More info: http://vuejs.org/guide/migration.html#Filters-Outside-Text-Interpolations
    
    77. Replace filtered value in v-html="'Secondly, &lt;b&gt;anyone who can use this browser can see and make changes to your work&lt;/b&gt;. So if you've got a nosy kid brother, look into setting up a separate profile for yourself.' | say" with a method or computed property
      Line 47: src/welcome/index.html
      Reason: Filters can now only be used inside text interpolations, e.g. {{ date | formatDate('YY-MM-DD') }}
      More info: http://vuejs.org/guide/migration.html#Filters-Outside-Text-Interpolations

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -9,6 +9,7 @@
 'use strict';
 const Vue = require('vue');
 const scroll = require('scroll');
+const locale = require('../locale');
 const { setPref } = require('../data/actions/pref');
 
 require('./index.less');
@@ -20,6 +21,18 @@ module.exports = Vue.extend({
 		/* How many sections are currently visible. */
 		shown: 1
 	}),
+
+	computed: {
+		noAccountNeeded() {
+			return locale.say("That means you don't need to create an account to use Twine 2, and everything you create isn't stored on a server somewhere else &mdash; it stays right in your browser.");
+		},
+		localStorage(){
+			return locale.say("Secondly, &lt;b&gt;anyone who can use this browser can see and make changes to your work&lt;/b&gt;. So if you've got a nosy kid brother, look into setting up a separate profile for yourself.");
+		},
+		thingsToRemember() {
+			return locale.say("Two &lt;b&gt;very important&lt;/b&gt; things to remember, though. Since your work is saved only in your browser, if you clear its saved data, then you'll lose your work! Not good. Remember to use that &lt;i class=\"fa fa-briefcase\"&gt;&lt;/i&gt;&nbsp;&lt;strong&gt;Archive&lt;/strong&gt; button often. You can also publish individual stories to files using the &lt;i class=\"fa fa-cog\"&gt;&lt;/i&gt; menu on each story in the story list. Both archive and story files can always be re-imported into Twine.");
+		}
+	},
 
 	methods: {
 		next() {
