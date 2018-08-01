12d6c0d78fdd20ab496117a78d269f085a4e4b55
Yonatan Kra <yonatan.kr@walkme.com>
2018-08-01 13:41:21 +0300

    Add firebase and travis to the mix

---
+++ src/common/firebase.js
@@ -0,0 +1,11 @@
+const firebase = require('firebase');
+// Initialize Firebase
+const config = {
+	apiKey: "AIzaSyAF5s_HGAi6DWNrfv_n5HdVRTZU0Q6aPA8",
+	authDomain: "none-linear-education.firebaseapp.com",
+	databaseURL: "https://none-linear-education.firebaseio.com",
+	projectId: "none-linear-education",
+	storageBucket: "none-linear-education.appspot.com",
+	messagingSenderId: "890714670008"
+};
+firebase.initializeApp(config);
