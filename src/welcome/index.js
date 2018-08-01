0ba720254ec1d7d8e50a23daf43def32c3a5eb79
Yonatan Kra <yonatan.kr@walkme.com>
2018-08-01 19:25:26 +0300

    Simple changes

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -4,12 +4,12 @@
 
  @class WelcomeView
  @extends Backbone.Marionette.ItemView
-**/
+ **/
 
 'use strict';
 const Vue = require('vue');
 const scroll = require('scroll');
-const { setPref } = require('../data/actions/pref');
+const {setPref} = require('../data/actions/pref');
 
 require('./index.less');
 
@@ -24,13 +24,13 @@ module.exports = Vue.extend({
 	methods: {
 		next() {
 			this.shown++;
-			
+
 			Vue.nextTick(() => {
 				scroll.top(
 					document.body,
 					document.querySelector(
-					'#welcomeView > div:last-of-type').offsetTop,
-					{ duration: 400 });
+						'#welcomeView > div:last-of-type').offsetTop,
+					{duration: 400});
 			});
 		},
 
@@ -46,3 +46,29 @@ module.exports = Vue.extend({
 		}
 	}
 });
+
+x = {
+	"id": "5be1fa91-dab5-4ff1-b6ed-8f22bad145b3",
+	"story": "67021b99-2db0-47b1-acc0-d7fc659516fd",
+	"top": 230.5,
+	"left": 562,
+	"width": 100,
+	"height": 100,
+	"tags": ["tag1"],
+	"name": "Start",
+	"selected": true,
+	"text": "Test\n\n[[test passage]]\n\n[[another test passage | 'Hey!!']]"
+}
+
+y = {
+	"id": "460373da-eca3-4137-a8a5-3c75f4b8c574",
+	"story": "67021b99-2db0-47b1-acc0-d7fc659516fd",
+	"top": 380.5,
+	"left": 487,
+	"width": 100,
+	"height": 100,
+	"tags": [],
+	"name": "test passage",
+	"selected": true,
+	"text": "Testing this one up"
+}
