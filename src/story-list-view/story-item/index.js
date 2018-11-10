6260b651de5292a78bdfd7b63763636d44e4eb8c
cpb8010 <cpb8010@gmail.com>
2018-11-10 09:50:59 -0600

    Remove zoom transition
    It's the last $mountTo and it's non functional

--- src/story-list-view/story-item/index.js
+++ src/story-list-view/story-item/index.js
@@ -6,7 +6,6 @@
 const moment = require("moment");
 const Vue = require("vue");
 const eventHub = require("../../common/eventHub");
-const ZoomTransition = require("../zoom-transition");
 
 require("./index.less");
 
@@ -48,30 +47,6 @@ module.exports = Vue.extend({
 		eventHub.$on("story-edit", id => {
 			if (this.story.id === id) {
 				this.edit();
-			}
-		});
-
-		// if we were previously editing a story, show a zoom shrinking back
-		// into us. The signature is a little bit different to save time; we
-		// know the ID of the story from the route, but don't have an object.
-
-		eventHub.$on("previously-editing", id => {
-			if (id === this.story.id) {
-				// The method for grabbing the page position of our element is
-				// cribbed from http://youmightnotneedjquery.com/.
-
-				let rect = this.$el.getBoundingClientRect();
-
-				/*
-				console.warn("story-item previously-editing using $mountTo");
-				new ZoomTransition({
-					data: {
-						reverse: true,
-						x: rect.left + (rect.right - rect.left) / 2,
-						y: rect.top + (rect.bottom - rect.top) / 2
-					}
-				}).$mountTo(document.body);
-				*/
 			}
 		});
 	},
