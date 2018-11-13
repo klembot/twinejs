9c321afdb4aba959d2b5e0fb427240f22606113a
cpb8010 <cpb8010@gmail.com>
2018-11-12 21:55:41 -0600

    Unescape GPL text
    The escaping isn't necessary with the v-html.
    There is more to do, but this makes it a little better.

--- src/dialogs/about/index.js
+++ src/dialogs/about/index.js
@@ -1,27 +1,30 @@
-const Vue = require('vue');
-const locale = require('../../locale');
+const Vue = require("vue");
+const locale = require("../../locale");
 
-require('./index.less');
+require("./index.less");
 
 module.exports = Vue.extend({
 	data: () => ({
 		origin: null
 	}),
 
-	template: require('./index.html'),
+	template: require("./index.html"),
 
 	computed: {
 		gplText() {
-			return locale.say(`This application is released under the \x3ca href="http:\/\/www.gnu.org/licenses/gpl-3.0.html">GPL v3\x3c/a> license, but any work created with it may be released under any terms, including commercial ones.`);
+			return locale.say(
+				`This application is released under the <a href="http:\/\/www.gnu.org/licenses/gpl-3.0.html">GPL v3</a> license, but any work created with it may be released under any terms, including commercial ones.`
+			);
 		},
 		fontsText() {
-			return locale.say(`Source Sans Pro and Source Code Pro were designed by Paul D. Hunt under the guidance of Robert Slimbach for \x3ca href="http:\/\/adobe.com/">Adobe\x3c/a>\x3cbr> Nunito was designed by \x3ca href="http:\/\/code.newtypography.co.uk/">Vernon Adams\x3c/a>`);
+			return locale.say(
+				`Source Sans Pro and Source Code Pro were designed by Paul D. Hunt under the guidance of Robert Slimbach for <a href="http:\/\/adobe.com/">Adobe</a><br> Nunito was designed by <a href="http:\/\/code.newtypography.co.uk/">Vernon Adams</a>`
+			);
 		}
-
 	},
 
 	components: {
-		'modal-dialog': require('../../ui/modal-dialog'),
+		"modal-dialog": require("../../ui/modal-dialog")
 	},
 
 	vuex: {
