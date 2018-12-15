b081b41bffcd9e4d759221369f57d576b283b0a6
Chris Klimas <chris@twinery.org>
2018-12-14 19:08:33 -0500

    FIx build problems

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ const config = (module.exports = {
 	module: {
 		rules: [
 			/*
+			Inline resources below 10k in size.
+			*/
+			{
+				test: /\.(eot|png|svg|ttf|woff|woff2)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: 'rsrc/[name].[hash].[ext]'
+				}
+			},
+
+			/*
 			We must exclude the top-level template, as I think the HTML plugin
 			is expecting a string as output, not a function.
 			*/
@@ -69,7 +81,7 @@ const config = (module.exports = {
 			buildNumber: require('./scripts/build-number').number,
 			inject: false,
 			minify: isRelease && {collapseWhitespace: true},
-			options: {cdn: useCdn}
+			cdn: useCdn
 		}),
 		new MiniCssExtractPlugin({filename: 'twine.css'}),
 		new PoPlugin({
@@ -95,55 +107,10 @@ if (isRelease) {
 		loader: 'babel-loader',
 		options: {presets: ['@babel/preset-env']}
 	});
-
-	/*
-	Base64 encode all fonts to work around a bug in NW.js -- see
-	https://github.com/nwjs/nw.js/issues/5080
-	*/
-
-	/* Stubbing out because base64-font-loader isn't Webpack 4 compatible
-	config.module.rules.push({
-		test: /\.(png|svg)(\?.*)?$/,
-		loader: 'url-loader',
-		options: {
-			limit: 10000,
-			name: 'rsrc/[name].[hash].[ext]'
-		}
-	});
-
-	** Don't do this is useCdn is true **
-
-	config.module.rules.push({
-		test: /\.(woff|woff2|ttf|eot|svg)(\?.*)?$/,
-		exclude: /img/,
-		loader: 'base64-font-loader'
-	});
-	*/
-
-	config.module.rules.push({
-		test: /\.(eot|png|svg|ttf|woff|woff2)(\?.*)?$/,
-		loader: 'url-loader',
-		options: {
-			limit: 10000,
-			name: 'rsrc/[name].[hash].[ext]'
-		}
-	});
-} else {
-	/*
-	Inline any resurces below 10k in size; otherwise, save it to a file.
-	*/
-	config.module.rules.push({
-		test: /\.(eot|png|svg|ttf|woff|woff2)(\?.*)?$/,
-		loader: 'url-loader',
-		options: {
-			limit: 10000,
-			name: 'rsrc/[name].[hash].[ext]'
-		}
-	});
 }
 
 if (useCdn) {
-	Object.assign(config.externals, {
+	config.externals = {
 		codemirror: 'CodeMirror',
 		/*
 		core-js has no external interface, so we borrow an existing global
@@ -159,5 +126,5 @@ if (useCdn) {
 		vue: 'Vue',
 		'vue-router': 'VueRouter',
 		vuex: 'Vuex'
-	});
+	};
 }
