2cc240ee8bb00dffe7848c34e3e670e3afc3c3f1
Chris Klimas <chris@twinery.org>
2019-03-03 20:26:42 -0500

    Fix issue with stories with spaces in their name
    Problem I think was router was re-encoding URL-encoded strings
    
    See https://github.com/klembot/twinejs/issues/514#issuecomment-466147986

--- src/data/id.js
+++ src/data/id.js
@@ -4,6 +4,9 @@ it is important that each story on disk have the same ID; however, in the web
 context, where everything is persisted to local storage instead, we simply need
 a unique ID. This function returns a deterministic ID in an Electron context,
 and a random ID in a web context.
+
+Consumers of this module should not depend on the exact ID, or the format of IDs
+returned -- this may change in future revisions.
 */
 
 const uuid = require('tiny-uuid');
@@ -11,7 +14,7 @@ const isElectron = require('../electron/is-electron');
 
 module.exports = function idFor(key) {
 	if (isElectron()) {
-		return window.encodeURIComponent(key);
+		return key;
 	}
 
 	return uuid();
