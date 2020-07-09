21b866fdafd356493eb6fb74a4cfdc68c7fa0d5d
Chris Klimas <chris@twinery.org>
2020-07-08 22:14:16 -0400

    Remove unneeded components

--- src/components/modal/__stories__/editor-modal.stories.js
+++
@@ -1,9 +0,0 @@
-import EditorModal from '../editor-modal';
-
-export default {title: 'Modal/<editor-modal>'};
-
-export const normal = () => ({
-	components: {EditorModal},
-	template:
-		'<editor-modal id="normal-editor-modal" :open="true"><template v-slot:title>Title</template><template v-slot:content>Content</template></editor-modal>'
-});
