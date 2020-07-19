508d56649938f150138742e3ff22f79ab07d347b
Chris Klimas <chris@twinery.org>
2020-07-19 15:28:02 -0400

    Rework appearance of story previews

--- src/components/story/story-list-item.vue
+++ src/components/story/story-list-item.vue
@@ -1,35 +1,35 @@
 <template>
 	<div class="story-list-item">
-		<div class="stack close vertical">
-			<story-preview @click="edit" :hue="0" :passages="story.passages" />
-			<div class="stack">
-				<h3 class="grow">{{ story.name }}</h3>
-				<anchored-paper
-					:allowFlip="true"
-					@click-away="toggleMenu"
-					position="bottom"
-					:visible="menuVisible"
-				>
-					<template v-slot:anchor>
-						<icon-button @click="toggleMenu" icon="settings" />
-					</template>
-					<template v-slot:paper>
-						<div class="stack vertical tight">
-							<icon-button icon="edit" label="common.edit" type="flat" />
-							<icon-button icon="play" label="common.play" type="flat" />
-							<icon-button icon="tool" label="common.test" type="flat" />
-							<icon-button
-								icon="download"
-								type="flat"
-								label="common.publishToFile"
-							/>
-							<icon-button icon="type" label="common.rename" type="flat" />
-							<icon-button icon="copy" label="common.duplicate" type="flat" />
-							<icon-button icon="trash-2" label="common.delete" type="flat" />
-						</div>
-					</template>
-				</anchored-paper>
-			</div>
+		<button class="preview-button" @click="edit" :style="previewButtonStyle">
+			<story-preview :name="story.name" :passages="story.passages" />
+		</button>
+		<div class="details">
+			{{ story.name }}
+			<anchored-paper
+				:allowFlip="true"
+				@click-away="toggleMenu"
+				position="bottom"
+				:visible="menuVisible"
+			>
+				<template v-slot:anchor>
+					<icon-button @click="toggleMenu" icon="settings" />
+				</template>
+				<template v-slot:paper>
+					<div class="stack vertical tight">
+						<icon-button icon="edit" label="common.edit" type="flat" />
+						<icon-button icon="play" label="common.play" type="flat" />
+						<icon-button icon="tool" label="common.test" type="flat" />
+						<icon-button
+							icon="download"
+							type="flat"
+							label="common.publishToFile"
+						/>
+						<icon-button icon="type" label="common.rename" type="flat" />
+						<icon-button icon="copy" label="common.duplicate" type="flat" />
+						<icon-button icon="trash-2" label="common.delete" type="flat" />
+					</div>
+				</template>
+			</anchored-paper>
 		</div>
 	</div>
 </template>
@@ -46,6 +46,23 @@ export default {
 		AnchoredPaper,
 		IconButton,
 		StoryPreview
+	},
+	computed: {
+		hue() {
+			let result = 0;
+
+			for (let i = 0; i < this.story.name.length; i++) {
+				result += this.story.name.charCodeAt(i);
+			}
+
+			return result % 360;
+		},
+		previewButtonStyle() {
+			return {
+				background: `linear-gradient(to bottom, hsl(${this.hue}, 90%, 95%), hsl(${this.hue}, 90%, 85%))`,
+				color: `hsla(${(this.hue + 45) % 360}, 90%, 40%, 0.25)`
+			};
+		}
 	},
 	data() {
 		return {menuVisible: false};
--- src/components/story/story-preview.vue
+++ src/components/story/story-preview.vue
@@ -1,35 +1,20 @@
 <template>
-	<button class="story-preview" @click="onClick" :style="style">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			version="1.1"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			:view-box.camel="svgViewBox"
-			v-html="svg"
-		/>
-	</button>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		version="1.1"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		:view-box.camel="svgViewBox"
+		v-html="svg"
+	/>
 </template>
 
 <script>
-import './story-preview.less';
-
 function passageRadius(length, longestLength) {
 	return (200 + 200 * (length / longestLength)) / 2;
 }
 
 export default {
 	computed: {
-		style() {
-			return {
-				background: `hsl(${this.hue}, 60%, 95%)`
-			};
-		},
-		passageStroke() {
-			return `hsl(${this.hue}, 90%, 45%)`;
-		},
-		passageFill() {
-			return `hsla(${this.hue}, 90%, 60%, 0.5)`;
-		},
 		longestPassageLength() {
 			return this.passages.reduce((result, current) => {
 				if (current.text.length > result) {
@@ -41,8 +26,7 @@ export default {
 		},
 		svg() {
 			if (this.passages.length <= 1) {
-				return `<circle cx="100" cy="100" r="75" fill="${this.passageFill}"
-					stroke="${this.passageStroke}" stroke-width="1px" />`;
+				return '<circle cx="100" cy="100" r="75" />';
 			}
 
 			return this.passages.reduce(
@@ -51,23 +35,18 @@ export default {
 					`<circle cx="${p.left + p.width / 2}"
 						cy="${p.top + p.height / 2}"
 						r="${passageRadius(p.text.length, this.longestPassageLength)}"
-						fill="${this.passageFill}"
-						stroke="${this.passageStroke}"
-						stroke-width="4px" />`,
+						 />`,
 				''
 			);
 		},
 		svgViewBox() {
 			if (this.passages.length <= 1) {
-				return '0 0 200 200';
+				return '25 25 150 150';
 			}
 
 			let minX = Number.POSITIVE_INFINITY;
-
 			let minY = Number.POSITIVE_INFINITY;
-
 			let maxX = Number.NEGATIVE_INFINITY;
-
 			let maxY = Number.NEGATIVE_INFINITY;
 
 			this.passages.forEach(p => {
@@ -95,15 +74,10 @@ export default {
 			return `${minX} ${minY} ${maxX - minX} ${maxY - minY}`;
 		}
 	},
-	methods: {
-		onClick() {
-			this.$emit('click');
-		}
-	},
 	name: 'story-preview',
 	props: {
-		hue: {type: Number, required: true},
-		passages: {type: Array, required: true}
+		name: {required: true, type: String},
+		passages: {required: true, type: Array}
 	}
 };
 </script>
