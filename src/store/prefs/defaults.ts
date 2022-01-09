ace06a4702fb836bb8aa2e6bdf643dbab5985dba
Chris Klimas <chris@twinery.org>
2022-01-09 14:30:46 -0500

    Update formats to latest, fix app icon in Windows

--- src/store/prefs/defaults.ts
+++ src/store/prefs/defaults.ts
@@ -24,7 +24,7 @@ export const defaults = (): PrefsState => ({
 	},
 	storyFormat: {
 		name: 'Harlowe',
-		version: '3.2.1'
+		version: '3.2.3'
 	},
 	storyFormatListFilter: 'current',
 	storyListSort: 'name',
