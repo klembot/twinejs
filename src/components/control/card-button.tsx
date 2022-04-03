cc4caf75af1127672282822f7eecfd3a4a37742e
Chris Klimas <chris@twinery.org>
2022-04-03 19:09:28 -0400

    Use <FocusTrap> for click handling

--- src/components/control/card-button.tsx
+++ src/components/control/card-button.tsx
@@ -4,6 +4,7 @@ import {CSSTransition} from 'react-transition-group';
 import {Card} from '../container/card';
 import {IconButton, IconButtonProps} from './icon-button';
 import './card-button.css';
+import FocusTrap from 'focus-trap-react';
 
 export interface CardButtonProps extends Omit<IconButtonProps, 'onClick'> {
 	/**
@@ -33,86 +34,7 @@ export const CardButton: React.FC<CardButtonProps> = props => {
 		null
 	);
 	const [cardEl, setCardEl] = React.useState<HTMLDivElement | null>(null);
-	const [previousFocusEl, setPreviousFocusEl] =
-		React.useState<HTMLElement | null>(null);
 	const {styles, attributes} = usePopper(buttonEl, cardEl, {strategy: 'fixed'});
-
-	// Move focus inside the card when it's opened.
-
-	React.useEffect(() => {
-		if (open && cardEl) {
-			setPreviousFocusEl(document.activeElement as HTMLElement | null);
-
-			// Use the focusSelector prop if that was specified;
-
-			if (focusSelector) {
-				const focusEl = cardEl.querySelector<HTMLElement>(focusSelector);
-
-				if (focusEl) {
-					// Select it if possible--e.g it's a text field.
-
-					if (typeof (focusEl as any).select === 'function') {
-						(focusEl as any).select();
-					}
-
-					focusEl.focus();
-					return;
-				}
-			}
-
-			// Prefer a text input if one's available.
-
-			const input =
-				cardEl.querySelector<HTMLInputElement>('input[type="text"]');
-
-			if (input) {
-				input.select();
-				input.focus();
-				return;
-			}
-
-			// Otherwise, choose the first button.
-
-			const button = cardEl.querySelector<HTMLButtonElement>('button');
-
-			if (button) {
-				button.focus();
-			}
-
-			// If neither is available, we've messed up--there needs to be at least
-			// one focusable element.
-		}
-	}, [cardEl, focusSelector, open]);
-
-	// Close an opened card if a click event occurs outside it.
-
-	React.useEffect(() => {
-		const closer = (event: MouseEvent) => {
-			let target: Node | null = event.target as HTMLElement;
-
-			while (target && target !== cardEl) {
-				target = target.parentNode;
-			}
-
-			if (target !== cardEl) {
-				onChangeOpen(false);
-			}
-		};
-
-		if (open) {
-			document.addEventListener('click', closer);
-			return () => document.removeEventListener('click', closer);
-		}
-	}, [cardEl, onChangeOpen, open, props]);
-
-	// Restore focus when the card is closed.
-
-	React.useEffect(() => {
-		if (!open && previousFocusEl) {
-			previousFocusEl.focus();
-			setPreviousFocusEl(null);
-		}
-	}, [open, previousFocusEl]);
 
 	return (
 		<span className="card-button">
@@ -128,16 +50,23 @@ export const CardButton: React.FC<CardButtonProps> = props => {
 				timeout={200}
 				unmountOnExit
 			>
-				<div
-					aria-label={ariaLabel}
-					className="card-button-card"
-					ref={setCardEl}
-					role="dialog"
-					style={styles.popper}
-					{...attributes.popper}
+				<FocusTrap
+					focusTrapOptions={{
+						clickOutsideDeactivates: true,
+						onDeactivate: () => onChangeOpen(false)
+					}}
 				>
-					<Card floating>{children}</Card>
-				</div>
+					<div
+						aria-label={ariaLabel}
+						className="card-button-card"
+						ref={setCardEl}
+						role="dialog"
+						style={styles.popper}
+						{...attributes.popper}
+					>
+						<Card floating>{children}</Card>
+					</div>
+				</FocusTrap>
 			</CSSTransition>
 		</span>
 	);
