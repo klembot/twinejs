7c27fc6cf20337292b28e31ed772bc572e761c09
Chris Klimas <chris@twinery.org>
2022-07-05 13:38:16 -0400

    Remove beta tag, add additional build archs

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -11,20 +11,20 @@ module.exports = {
 	},
 	files: ['electron-build/**/*', 'node_modules/**/*'],
 	linux: {
-		artifactName: `twine-${pkg.version}-linux-\${arch}.zip`,
-		target: [{arch: ['ia32', 'x64'], target: 'zip'}]
+		artifactName: `Twine ${pkg.version} (Linux \${arch}).zip`,
+		target: [{arch: ['arm64', 'ia32', 'x64'], target: 'zip'}]
 	},
 	mac: {
-		artifactName: `twine-${pkg.version}-macos.dmg`,
+		artifactName: `Twine ${pkg.version} (macOS).dmg`,
 		icon: `icons/app-${isPreview ? 'preview' : 'release'}.png`,
-		target: 'dmg'
+		target: {arch: ['universal'], target: 'dmg'}
 	},
 	nsis: {
 		oneClick: false,
 		allowToChangeInstallationDirectory: true
 	},
 	win: {
-		artifactName: `twine-${pkg.version}-windows.exe`,
+		artifactName: `Twine ${pkg.version} (Windows).exe`,
 		icon: `icons/app-${isPreview ? 'preview' : 'release'}.ico`,
 		target: 'nsis'
 	}
