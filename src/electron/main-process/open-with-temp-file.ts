72f268aa7169dde3f285db45f649f1459df75eb8
Lorenzo Ancora <34890309+LorenzoAncora@users.noreply.github.com>
2023-09-03 02:11:56 +0200

    Automatic cleaning of obsolete temporary files
    When generating a temp file to be opened by the default web browser for story testing, delete the previous one by default. In addition, clear the leftovers before the process is terminated.

--- src/electron/main-process/open-with-temp-file.ts
+++ src/electron/main-process/open-with-temp-file.ts
@@ -1,11 +1,30 @@
 import {app, shell} from 'electron';
-import {writeFile} from 'fs-extra';
+import { writeFile, remove } from 'fs-extra';
 import path from 'path';
 import uuid from 'tiny-uuid';
 
-export async function openWithTempFile(data: string, suffix: string) {
-	const tempPath = path.join(app.getPath('temp'), uuid() + suffix);
+let oldtmp:string|null = null;
 
+function deleteTempFile(path: string|null, required_subroot: string) {
+	if (path != null && path.startsWith(required_subroot)) {
+		remove(path, () => {
+			oldtmp = null;
+			console.debug(`Temporary file ${path} deleted.`);
+		});
+	}
+}
+
+export async function openWithTempFile(data: string, suffix: string, delete_next: boolean = true) {
+	const tempSubRoot: string = app.getPath('temp');
+	const tempPath: string = path.join(tempSubRoot, uuid() + suffix);
+
+	deleteTempFile(oldtmp, tempSubRoot)
 	await writeFile(tempPath, data);
+	if (delete_next === true) oldtmp = tempPath;
 	shell.openPath(tempPath);
 }
+
+app.on('will-quit', () => {
+	const tempSubRoot: string = app.getPath('temp');
+	deleteTempFile(oldtmp, tempSubRoot);
+});
