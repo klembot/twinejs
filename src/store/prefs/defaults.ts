ee893e52ccdf4d5ec6461204022aaba2706c4a9b
Leon <L_1_L_0@yahoo.com>
2024-01-03 20:58:45 +1000

    Updated Harlowe to 3.3.8.

--- src/store/prefs/defaults.ts
+++ src/store/prefs/defaults.ts
@@ -25,7 +25,7 @@ export const defaults = (): PrefsState => ({
 	},
 	storyFormat: {
 		name: 'Harlowe',
-		version: '3.3.7'
+		version: '3.3.8'
 	},
 	storyFormatListFilter: 'current',
 	storyListSort: 'name',
