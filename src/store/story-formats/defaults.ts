b3754cf5cb667ce2eba2957795358e601dff253e
Thomas M. Edwards <thomasmedwards@gmail.com>
2024-07-18 12:13:16 -0500

    feat: update SugarCube to v2.37.0

--- src/store/story-formats/defaults.ts
+++ src/store/story-formats/defaults.ts
@@ -47,7 +47,7 @@ export const builtins = () => [
 	},
 	{
 		name: 'SugarCube',
-		url: 'story-formats/sugarcube-2.36.1/format.js',
-		version: '2.36.1'
+		url: 'story-formats/sugarcube-2.37.0/format.js',
+		version: '2.37.0'
 	}
 ];
