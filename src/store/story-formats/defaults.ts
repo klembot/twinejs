d265058d589a0a95e9ebc89c343c5980beb4e5e9
Chris Klimas <hello@chrisklimas.com>
2024-11-24 15:18:43 -0500

    Add Chapbook 2.3.0

--- src/store/story-formats/defaults.ts
+++ src/store/story-formats/defaults.ts
@@ -6,8 +6,8 @@ export const builtins = () => [
 	},
 	{
 		name: 'Chapbook',
-		url: 'story-formats/chapbook-2.2.0/format.js',
-		version: '2.2.0'
+		url: 'story-formats/chapbook-2.3.0/format.js',
+		version: '2.3.0'
 	},
 	{
 		name: 'Harlowe',
