0736edb685320dc0e97930e3d64f8da8b1126fdc
Bengeljo <86926513+Bengeljo@users.noreply.github.com>
2026-03-05 23:58:59 +0100

    Removed the passage-tag removal from story-tag removal commit

--- src/store/stories/action-creators/delete-tag-from-all-stories.ts
+++ src/store/stories/action-creators/delete-tag-from-all-stories.ts
@@ -1,8 +1,8 @@
 import {Thunk} from 'react-hook-thunk-reducer';
-import {StoriesAction, StoriesState, Story, Passage} from '../stories.types';
+import {StoriesAction, StoriesState, Story} from '../stories.types';
 
 /**
- * Deletes a tag from all stories and all their passages globally.
+ * Deletes a tag from all stories (story-level tags only, not passage tags or colors).
  */
 export function deleteTagFromAllStories(
 	stories: Story[],
@@ -18,35 +18,6 @@ export function deleteTagFromAllStories(
 					props: {
 						tags: story.tags.filter(tag => tag !== tagName)
 					}
-				});
-			}
-
-			// Remove tag from tagColors if present
-			if (story.tagColors && story.tagColors[tagName]) {
-				const tagColors = {...story.tagColors};
-				delete tagColors[tagName];
-				dispatch({
-					type: 'updateStory',
-					storyId: story.id,
-					props: {tagColors}
-				});
-			}
-
-			// Remove tag from all passages in this story
-			const passageUpdates: Record<string, Partial<Passage>> = {};
-			story.passages.forEach(passage => {
-				if (passage.tags.includes(tagName)) {
-					passageUpdates[passage.id] = {
-						tags: passage.tags.filter(tag => tag !== tagName)
-					};
-				}
-			});
-
-			if (Object.keys(passageUpdates).length > 0) {
-				dispatch({
-					type: 'updatePassages',
-					storyId: story.id,
-					passageUpdates
 				});
 			}
 		});
